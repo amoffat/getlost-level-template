4f8e690bc1a58ded436ad41c76ae28bac0d15b62
Andrew Moffat <arwmoffat@gmail.com>
2025-03-04 11:47:34 -0800

    better port vis

--- .devcontainer/hooks/postStart.sh
+++ .devcontainer/hooks/postStart.sh
@@ -6,4 +6,5 @@ npx pm2 start --name tiled 'tiled level/tiled/level.tiled-project' --stop-exit-c
 
 # One day we'll be able to do this declaratively
 # https://github.com/orgs/community/discussions/4068
-gh codespace ports visibility 5173:public -c $CODESPACE_NAME
+# We sleep to give the server time to start up :|
+(sleep 3 && gh codespace ports visibility 5173:public -c $CODESPACE_NAME) &
