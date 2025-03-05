d857780c8f88259ecbd51aadabc94fb2f6e5419e
Andrew Moffat <arwmoffat@gmail.com>
2025-03-04 18:24:20 -0800

    port visibility

--- .devcontainer/hooks/postAttach.sh
+++ .devcontainer/hooks/postAttach.sh
@@ -1,2 +1,12 @@
 #!/usr/bin/env bash
 set -eux
+
+PUBLIC_PORT=5173
+
+while ! gh codespace ports -c $CODESPACE_NAME | grep -q "$PUBLIC_PORT"; do
+  sleep 1
+done
+
+# One day we'll be able to do this declaratively
+# https://github.com/orgs/community/discussions/4068
+gh codespace ports visibility "$PUBLIC_PORT:public" -c $CODESPACE_NAME
--- .devcontainer/hooks/postCreate.sh
+++ .devcontainer/hooks/postCreate.sh
@@ -1,6 +1,2 @@
 #!/usr/bin/env bash
 set -eux
-
-# One day we'll be able to do this declaratively
-# https://github.com/orgs/community/discussions/4068
-gh codespace ports visibility 5173:public -c $CODESPACE_NAME
