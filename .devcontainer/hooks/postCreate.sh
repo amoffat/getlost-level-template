91395c8655ede9b353dea0e0a5f242f7411b48c8
Andrew Moffat <arwmoffat@gmail.com>
2025-03-08 20:22:41 +0000

    fix

--- .devcontainer/hooks/postCreate.sh
+++ .devcontainer/hooks/postCreate.sh
@@ -2,7 +2,7 @@
 set -eux
 
 # In a Github Codespace, using --host leads to a 502 bad gateway error
-if [ -n "$CODESPACE_NAME" ]; then
+if [ -n "${CODESPACE_NAME:-}" ]; then
     vite_cmd="npx vite"
 # In a local devcontainer, using --host is necessary to access the dev server
 # from the browser
