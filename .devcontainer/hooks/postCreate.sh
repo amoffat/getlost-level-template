be50ed9accf93b3f391815a467415db86a90814e
Andrew Moffat <arwmoffat@gmail.com>
2025-03-08 04:14:36 +0000

    create, then start

--- .devcontainer/hooks/postCreate.sh
+++ .devcontainer/hooks/postCreate.sh
@@ -1,2 +1,5 @@
 #!/usr/bin/env bash
 set -eux
+
+npx pm2 start --name vite "npx vite --host" --restart-delay=1000
+npx pm2 start --name tiled 'tiled level/tiled/level.tiled-project' --stop-exit-codes 0
