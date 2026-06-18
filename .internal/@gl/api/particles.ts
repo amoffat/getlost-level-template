c5910c2a1c73fc6b9aaf3ea8f09d8534368af7ea
Andrew Moffat <arwmoffat@gmail.com>
2026-06-18 08:17:55 +0000

    particles

--- .internal/@gl/api/particles.ts
+++ .internal/@gl/api/particles.ts
@@ -1,6 +1,19 @@
-export declare function create(
-  ptr: number,
-  tileset: string,
-  tileidx: number,
-  num: number
-): void;
+import { Vector2 } from "@gl/types/api/vector";
+
+interface SnowOpts {
+  turbulence?: number;
+  dampening?: number;
+  wind?: Vector2;
+}
+
+export declare function createSnow({
+  tilesetId,
+  tileId,
+  num,
+  opts,
+}: {
+  tilesetId: string;
+  tileId: string;
+  num: number;
+  opts?: SnowOpts;
+}): void;
